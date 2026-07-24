edf7d6261303a6125f096bcc5996b9d06e2a1d8a
pinkcollarsocials <pinkcollarsocials@gmail.com>
2026-07-23 20:20:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,38 @@ const navLinks = document.querySelector(".nav-links");
 
 
 menuToggle.addEventListener("click", () => {
-
     navLinks.classList.toggle("active");
+});
+
+
+
+const sections = document.querySelectorAll("section");
+
+
+const observer = new IntersectionObserver(
+    entries => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+
+                entry.target.classList.add("show");
+
+            }
+
+        });
+
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+
+sections.forEach(section => {
+
+    section.classList.add("hidden");
+
+    observer.observe(section);
 
 });
